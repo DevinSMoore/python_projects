08ba5be0071e2085e6b1bc987ce29dea8d6ad3c1
DevinSMoore <moore.ds04@gmail.com>
2022-06-09 13:22:32 -0700

    Base files for tracker.

--- money_tracker/test.py
+++ money_tracker/test.py
@@ -1,3 +1,4 @@
+from cmath import exp
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass as dc
@@ -8,11 +9,23 @@ class item_category(enum.Enum):
     alcohol = 1
     weed    = 2
     monthly = 3
-    
-df = pd.DataFrame([[600,0]], index=[1], columns=["income", "expenses"])
 
-print(df)
+column_names = ["income", "expenses"]
+#index_names 
+df = pd.DataFrame([[600,0]], columns=column_names)
+#df.reset_index(inplace=True)
+print('Before concat:\n', df, '\n')
 
-df.add(5,1)
+income = input("Please enter your income: ")
+expenses = input("Please enter your expenses: ")
 
-print(df)
+i_df = pd.DataFrame([[income, expenses]], columns=column_names)
+
+
+
+print("Index of df: ", df.index, "\nIndex of i_df: ", i_df.index)
+
+
+df = pd.concat([df, i_df], ignore_index=True, axis=0)
+
+print('After concat:\n', df, '\n')
